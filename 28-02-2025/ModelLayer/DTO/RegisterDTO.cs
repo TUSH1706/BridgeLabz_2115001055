8e44b9e5aa06716202bf34774d017a2aa1d46a40
tushar singh <tusharsingh0602@gmail.com>
2025-03-10 21:23:16 +0530

    Assignment

---
+++ 28-02-2025/ModelLayer/DTO/RegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelLayer.DTO
+{
+    public class RegisterDTO
+    {
+        public string firstName {  get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+
+
+    }
+}
